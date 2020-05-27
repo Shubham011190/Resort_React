d9a1085968aa53ecdab2ec868c0e4508662e14a4
Shubham Singhal <shubham.011190@gmail.com>
2020-05-27 20:59:38 +0530

    Rout added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Home from './pages/Home';
 import Error from './pages/Error';
 import SingleRoom from './pages/SingleRoom';
 import Rooms from './pages/Rooms';
+import { BrowserRouter as Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/rooms' component = {Rooms} />
+      <Route exact path='/single-room' component = {SingleRoom} />
       </>
 
   );
